6485e831a29b9a2dbac094b668b88380d55e67cb
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-01 22:45:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,6 @@ end
 
 def take_a_number(array, name)
   array << name
-  number = array.index("name")
+  number = array.index("name") + 1
   puts "Welcome #{name}. You are number #{number} in line."
 end
