c0c8530e29b37aa74aed185d045f191d420b37ba
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-01 23:20:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
     puts "The line is currently empty."
   else
     array.each do |person|
-      person = "#{array.index("person") + 1}. person"
+      person = "#{array.index("person").to_i + 1}. person"
 
     end
     new_list = array.join(" ")
