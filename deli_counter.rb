372ec1183e424d75996dafa2f66e1363c0bba9b0
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-01 22:35:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(array)
     puts "The line is currently: " + new_list
 end
 
-def take_a_number
+def take_a_number(array, name)
   array << name
   puts "Welcome #{name}. You are number #{name.index + 1} in line."
 end
