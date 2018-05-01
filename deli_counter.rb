5fe0dbb52039b4508aec80073580d7c13f7b7441
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-01 22:03:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,12 @@ katz_deli = []
 
 
 def line(array)
-  reactive_line = []
   if array.length == 0
     puts "The line is currently empty."
   else
     array.each do |person|
      spot = person.index += 1
-     reactive_line << spot
     end
-    puts "The line is currently: 1. #{reactive_line}"
+    puts "The line is currently:" + spot + person
   end
 end
