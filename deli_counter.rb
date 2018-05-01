b1c6d994b980fc4e13454b4bbad8d67a7968514e
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-01 22:14:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-        new_list = array.map.with_index {|name, index| "#{index+1}. #{name}"}  
-    end
+        new_list = array.map.with_index {|name, index| "#{index+1}. #{name}"}
+
     puts "The line is currently:" + new_list
   end
 end
